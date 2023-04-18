010ffed7e5828f5b63721d04ca06378a5a7d8007
Mohammad Atif Quamar <atif20523@iiitd.ac.in>
2023-04-18 21:32:43 +0530

    User Authentication Done

--- app/src/main/java/com/example/tweeklabs/LoginActivity.kt
+++ app/src/main/java/com/example/tweeklabs/LoginActivity.kt
@@ -1,12 +1,15 @@
 package com.example.tweeklabs
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Spannable
 import android.text.SpannableString
 import android.text.style.ForegroundColorSpan
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 
 class LoginActivity : AppCompatActivity() {
@@ -40,5 +43,33 @@ class LoginActivity : AppCompatActivity() {
         spannableTextLabs.setSpan(ForegroundColorSpan(themeColor), 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
         tvLabs.text = spannableTextLabs
 
+        btnSignIn.setOnClickListener {
+            val inputPhoneNumber = etPhoneNumber.text.toString()
+            val inputPassword = etPassword.text.toString()
+            if(inputPhoneNumber == ""){
+                Toast.makeText(this, "Phone Number can't be Blank.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if(inputPassword == ""){
+                Toast.makeText(this, "Password can't be Blank.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val isUserAuthenticated = userAuthentication(inputPhoneNumber, inputPassword)
+            if (isUserAuthenticated){
+                Log.i("Authentication Status", "User is Valid.")
+                Toast.makeText(this, "Success!", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, AthletePageActivity::class.java)
+                startActivity(intent)
+            }
+            else {
+                Log.i("Authentication Status", "User is Invalid.")
+                Toast.makeText(this, "Invalid Credentials.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+        }
+    }
+
+    private fun userAuthentication(phoneNumber: String, password: String): Boolean {
+        return (phoneNumber == "9876543210" && password == "9876543210")
     }
 }
