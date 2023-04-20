bd59eb131471168b3efc8f597a9cd713a77319e1
Mohammad Atif Quamar <atif20523@iiitd.ac.in>
2023-04-20 17:54:51 +0530

    App functionalities all done. May improve UI

--- app/src/main/java/com/example/tweeklabs/AthletePageActivity.kt
+++ app/src/main/java/com/example/tweeklabs/AthletePageActivity.kt
@@ -3,6 +3,9 @@ package com.example.tweeklabs
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tweeklabs.adapters.AthleteSessionsAdapter
 
 
 class AthleteSession(
@@ -26,9 +29,6 @@ class AthletePageActivity : AppCompatActivity() {
     private lateinit var bowlerType: TextView
     private lateinit var totalBallsBowled: TextView
     private lateinit var countSessions: TextView
-    private lateinit var sessionBallsCount: TextView
-    private lateinit var sessionAverageSpeed: TextView
-    private lateinit var sessionTime: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,19 +38,23 @@ class AthletePageActivity : AppCompatActivity() {
         bowlerType = findViewById(R.id.tv_bowler_type)
         totalBallsBowled = findViewById(R.id.tv_count_balls)
         countSessions = findViewById(R.id.tv_count_sessions)
-        sessionBallsCount = findViewById(R.id.tv_value_balls_count)
-        sessionAverageSpeed = findViewById(R.id.tv_value_av_speed)
-        sessionTime = findViewById(R.id.tv_value_date)
 
         // Dummy Data Created
         val session1 = AthleteSession(1, 65, 124.04, "13/03/2023, 10:24AM")
-        val listOfSessions = listOf(session1)
+        val session2 = AthleteSession(2, 98, 135.20, "14/03/2023, 05:35PM")
+        val session3 = AthleteSession(1, 65, 124.04, "17/03/2023, 07:44PM")
+        val listOfSessions = listOf(session1, session2, session3)
         val personData = PlayerInformation("Atif Quamar", "Medium-Fast", 650, 14, listOfSessions)
 
         bowlerName.text = personData.name
         bowlerType.text = personData.bowlerType
         totalBallsBowled.text = personData.totalBallsBowled.toString()
         countSessions.text = personData.totalSessions.toString()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.sessions_recyclerview)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = AthleteSessionsAdapter(listOfSessions)
+        recyclerView.adapter = adapter
 
 
     }
---
+++ app/src/main/java/com/example/tweeklabs/adapters/AthleteSessionsAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.tweeklabs.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.tweeklabs.AthleteSession
+import com.example.tweeklabs.R
+
+class AthleteSessionsAdapter (private val allSessions: List<AthleteSession>):
+    RecyclerView.Adapter<AthleteSessionsAdapter.SessionInformationViewHolder>() {
+
+    class SessionInformationViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var sessionId: TextView = itemView.findViewById(R.id.tv_session_number)
+        var sessionBallsCount: TextView = itemView.findViewById(R.id.tv_value_balls_count)
+        var sessionAverageSpeed: TextView = itemView.findViewById(R.id.tv_value_av_speed)
+        var sessionTime: TextView = itemView.findViewById(R.id.tv_value_date)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SessionInformationViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.sessions_cardview, parent, false)
+        return SessionInformationViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: SessionInformationViewHolder, position: Int) {
+        val item = allSessions[position]
+        holder.sessionId.text = "Session " + item.sessionId.toString()
+        holder.sessionBallsCount.text = item.ballsBowled.toString()
+        holder.sessionAverageSpeed.text = item.averageSpeed.toString()
+        holder.sessionTime.text = item.time
+    }
+
+    override fun getItemCount(): Int = allSessions.size
+}
